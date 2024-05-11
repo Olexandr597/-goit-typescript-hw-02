0e4a52d207a2f4581af348d69b880dd4eb2b0129
Olexandr597 <Olexandr597@gmail.com>
2024-05-11 19:23:10 +0300

    up

---
+++ src/components/ImageModal/ImageModal.tsx
@@ -0,0 +1,53 @@
+import Modal from "react-modal";
+import css from "./ImageModal.module.css";
+import { Image } from "../../types";
+
+
+interface ImageModalProps {
+  closeModal: () => void;
+  modalIsOpen: boolean;
+  modalImage: Image | null;
+}
+
+const customStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
+const ImageModal: React.FC<ImageModalProps> = ({ closeModal, modalIsOpen, modalImage }) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      {modalImage && (
+        <div className={css.modalContainer}>
+          <div className={css.imgContainer}>
+            <img
+              className={css.modalImg}
+              src={modalImage.urls.regular}
+              alt={modalImage.slug}
+            />
+          </div>
+          <div className={css.descContainer}>
+            <p className={css.imgDesc}>{modalImage.alt_description}</p>
+            <p className={css.imgLikes}>👍: {modalImage.likes}</p>
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
+export default ImageModal;
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,49 @@
+import css from "./SearchBar.module.css";
+
+interface SearchBarProps {
+  onSetSearchQuery: (query: string) => void;
+  toast: (message: string) => void;
+}
+
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSetSearchQuery, toast }) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const value = (e.target as HTMLFormElement).search.value;
+    if (value.trim() === "") {
+      toast("Please enter text to search for images!"
+      // {
+      //   icon: "👀",
+      //   style: {
+      //     borderRadius: "10px",
+      //     background: "#333",
+      //     color: "#fff",
+      //   },
+      // }
+      );
+
+      return;
+    }
+    onSetSearchQuery(value.trim());
+    (e.target as HTMLFormElement).reset();
+  };
+  return (
+    <header className={css.header}>
+      <form className={css.searchForm} onSubmit={onSubmit}>
+        <input
+          className={css.searchField}
+          name="search"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button className={css.searchBtn} title="Pres for search" type="submit">
+          🔎
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
